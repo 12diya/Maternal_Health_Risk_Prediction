e23a4f5231616f9901501bc46de83b6a75439fc8
Janvi <janvimalhotra981@gmail.com>
2023-01-09 16:36:33 +0530

    Merge

--- main.js
+++
@@ -1,7 +0,0 @@
-// add all the javascript code here
-var toggle = document.getElementById("toggle");
-var container = document.getElementById("container");
-
-toggle.onclick = function(){
-	container.classList.toggle('active');
-}
